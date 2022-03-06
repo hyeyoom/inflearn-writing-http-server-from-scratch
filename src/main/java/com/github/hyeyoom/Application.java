9d3c992037fad2a8d4e4ac7dc4eaea325d89d131
hyeyoom <neoul_chw@icloud.com>
2022-03-06 23:05:27 +0900

    3.2. 과제 시작

--- src/main/java/com/github/hyeyoom/Application.java
+++ src/main/java/com/github/hyeyoom/Application.java
@@ -2,16 +2,25 @@ package com.github.hyeyoom;
 
 import java.io.*;
 
+/**
+ * 3.2. 과제
+ *
+ * 읽어온 데이터를 요청 메세지의 형식에 맞게 다음과 같이 출력해봅시다._.
+ * 1. 요청라인
+ *  - method
+ *  - URI
+ *  - protocol
+ * 2. Headers
+ * 3. Body
+ */
 public class Application {
     public static void main(String[] args) throws IOException {
-        // 1. 파일스트림에서 파일을 로드해봅시다.
         final File fileToRead = new File("http_message.txt");
         try (InputStream inputStream = new FileInputStream(fileToRead)) {
             final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
             String line;
             while ((line = br.readLine()) != null) {
-                // 2. 로드한 내용을 화면에 출력해봅시다.
                 System.out.println("line = " + line);
             }
         }
