12ef0111b8904417727e93f021cfb61bf898603d
hyeyoom <neoul_chw@icloud.com>
2022-03-06 22:36:58 +0900

    3.1. done

--- src/main/java/com/github/hyeyoom/Application.java
+++ src/main/java/com/github/hyeyoom/Application.java
@@ -1,11 +1,19 @@
 package com.github.hyeyoom;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         // 1. 파일스트림에서 파일을 로드해봅시다.
-        // 2. 로드한 내용을 화면에 출력해봅시다.
-        // 읽을 파일은 프로젝트 루트의 http_message.txt 파일을 참고하세요.
+        final File fileToRead = new File("http_message.txt");
+        try (InputStream inputStream = new FileInputStream(fileToRead)) {
+            final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                // 2. 로드한 내용을 화면에 출력해봅시다.
+                System.out.println("line = " + line);
+            }
+        }
     }
 }
