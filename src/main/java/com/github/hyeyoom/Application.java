ccd0d36e069cafe4632a2c7d2f5035eaedda2255
hyeyoom <neoul_chw@icloud.com>
2022-03-06 21:53:44 +0900

    3.1. start

--- src/main/java/com/github/hyeyoom/Application.java
+++ src/main/java/com/github/hyeyoom/Application.java
@@ -1,26 +1,11 @@
 package com.github.hyeyoom;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        // 1. 브라우저의 요청을 8080으로 받고
-        final ServerSocket serverSocket = new ServerSocket(8080);
-        try (Socket socket = serverSocket.accept()) {
-            try (InputStream inputStream = socket.getInputStream()) {
-                final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line = null;
-                while (!"".equals(line = br.readLine())) {
-                    // 2. 문자열로 바꾸어 출력해보세요
-                    System.out.println("line = " + line);
-                }
-            }
-        }
+        // 1. 파일스트림에서 파일을 로드해봅시다.
+        // 2. 로드한 내용을 화면에 출력해봅시다.
+        // 읽을 파일은 프로젝트 루트의 http_message.txt 파일을 참고하세요.
     }
 }
